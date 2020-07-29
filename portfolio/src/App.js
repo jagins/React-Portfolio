18586bb40e1e76dde8aee98e2f0dd32089049511
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-07-29 14:37:36 -0500

    starting portfolio over

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,40 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import ReactGA from 'react-ga';
-import axios from 'axios';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Portfolio from './Components/Portfolio';
 
-function App() 
-{
-  const [state, setState] = useState({resumeData: {}})
-
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
-
-  useEffect(() => {
-    axios.get('/resumeData.json')
-    .then(res => {
-      setState({resumeData: res.data})
-    })
-    .catch(err => {
-      console.log(err);
-      alert(err);
-    })
-  }, [])
-
-    return (
-      <div className="App">
-        <Header data={state.resumeData.main}/>
-        <About data={state.resumeData.main}/>
-        <Resume data={state.resumeData.resume}/>
-        <Portfolio data={state.resumeData.portfolio}/>
-        <Footer data={state.resumeData.main}/>
-      </div>
-    );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
