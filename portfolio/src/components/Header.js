c1fd13d4db563473b32cd3bbab7d7b6fb8b6c6cd
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-07-30 12:25:09 -0500

    about me section almost done styling

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {FaLinkedinIn, FaGithub, FaArrowCircleDown} from 'react-icons/fa';
+import {FaLinkedinIn, FaGithub} from 'react-icons/fa';
+
+import {IconContext} from 'react-icons';
 
 function Header()
 {
     return (
-        <section>
+        <section id='home'>
             <nav>
                 <a href='#'>Home</a>
                 <a href='#'>About</a>
@@ -13,11 +15,14 @@ function Header()
             </nav>
 
             <h1>I'm Josh Agins</h1>
-            <h3>Full Stack Web Developer skilled in JavaScript, React, Node, and Express. Passionate about learning new technology, writing clean, easy to read code, and enjoy putting the code together to make something from an idea to an app that a user can use and interact with.</h3>
-            <FaLinkedinIn/>
-            <FaGithub/>
-
-            <FaArrowCircleDown/>
+            <h3>Full Stack Web Developer skilled in JavaScript, React, Node, and Express. Passionate about learning new technology, writing clean, easy to read code, and enjoy putting the code together to make something from an idea to an app that a user can use and interact with to have a good user experience.</h3>
+            
+            <IconContext.Provider value={{className: 'icons', size: '30px'}}>
+                <div className='icons'>
+                    <FaLinkedinIn/>
+                    <FaGithub/>
+                </div>
+            </IconContext.Provider>
         </section>
     )
 }
