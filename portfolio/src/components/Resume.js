adf0495724c179c37d8ca48a15ce3103a2556edf
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-07-30 16:56:33 -0500

    styling work section almost done

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -3,23 +3,41 @@ import React from 'react';
 function Resume()
 {
     return (
-        <section>
-            <h3>EDUCATION</h3>
-            <h2>Lambda School</h2>
-            <p>Full Stack Web Development</p>
-            <p>August 2020</p>
-            <p>Full-time 40 week Full Stack Web Development program that teaches front and backend development using HTML5, CSS3, ES6 JavaScript, React.js, Node.js, Express.js, RESTFUl API, Git, Github, and computer science in Python including topics such as Object Oriented Programming (OOP), Algorithms, Data Structures, Hash Tables, Graphs, and Computer Architecture.</p>
+        <section id='resume'>
+            <div className='education'>
+                <h3 className='education-header'>EDUCATION</h3>
+                <h2>Lambda School</h2>
+            </div>
+            
+            <div className='education-content'>
+                <div className='graduation'>
+                    <p className='title'>Full Stack Web Development</p>
+                    <p className='grad-date'>August 2020</p>
+                </div>
+                
+                <p className='education-desc'>Full-time 40 week Full Stack Web Development program that teaches front and backend development using HTML5, CSS3, ES6 JavaScript, React.js, Node.js, Express.js, RESTFUl API, Git, Github, and computer science in Python including topics such as Object Oriented Programming (OOP), Algorithms, Data Structures, Hash Tables, Graphs, and Computer Architecture.</p>
+               
+                <h2 className='school'>DeVry University - Addison</h2>
+               
+               <div className='devry'>
+                    <p className='title'>Game and Simulation Programming</p>
+                    <p className='grad-date'>June 2013</p>
+               </div>
+                
+                <p className='education-desc'>Bachelor of Science program that teaches game design and game and simulation programming using C++/C# and DirectX. Worked with game engines such as Unreal3 and Unity and also creating a custom game engine from scratch. 3d modeling using 3D Studio Max was also covered during the 127 credit hour cirriculum.</p>
+            </div>
 
-            <h2>DeVry University</h2>
-            <p>Game and Simulation Programming</p>
-            <p>June 2013</p>
-            <p>Bachelor of Science program that teaches game design and game and simulation programming using C++/C# and DirectX. Worked with game engines such as Unreal3 and Unity and also creating a custom game engine from scratch. 3d modeling using 3D Studio Max was also covered during the 127 credit hour cirriculum.</p>
-
-            <h3>WORK</h3>
-            <h2>NetherRealm Studios</h2>
-            <p>Mobile QA Analyst</p>
-            <p>June 2015 - June 2017</p>
-            <p>Worked on titles such as Mortal Kombat X, WWE Immortals, Injustice: Gods Among Us, and Injustice 2 on iOS and Android. Resonsilbilties were to find and document bugs to the QA Lead and in JIRA. Ensured high quality standards and intregrity of the product. Worked on a taem of 7 other Mobile QA Analysts</p>
+            <div className='work'>
+                <h3 className='work-header'>WORK</h3>
+                <h2 class='work-title'>NetherRealm Studios</h2>
+            </div>
+            
+            <div className='job'>
+                <p className='job-title'>Mobile QA Analyst</p>
+                <p className='job-date'>June 2015 - June 2017</p>
+            </div>
+           
+            <p className='work-desc'>Worked on titles such as Mortal Kombat X, WWE Immortals, Injustice: Gods Among Us, and Injustice 2 on iOS and Android. Resonsilbilties were to find and document bugs to the QA Lead and in JIRA. Ensured high quality standards and intregrity of the product. Worked on a team of 7 other Mobile QA Analysts.</p>
 
             <h2>WMS Gaming Inc</h2>
             <p>Test Engineer</p>
