856120b187acf943deda131ac018959f68a15def
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-07-31 12:27:17 -0500

    project section laid out need to tweak text on the images

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import earthmed from '../images/portfolio/earthmed.JPG';
 import disney from '../images/portfolio/disney.png';
+import animals from '../images/portfolio/animals.JPG';
+
 function Projects()
 {
     return (
-        <section>
-            <img src={disney} alt='Disney Kids API project'/>
+        <section id='projects'>
+            <h3>CHECK OUT SOME OF MY WORK</h3>
+            <div className='project-container'>
+                <div className='wrapper'>
+                    <p>hello</p>
+                   <img src={earthmed} alt='Earthmed Project'/>
+                </div>
+                
+                <div className='wrapper'>
+                    <p>hello</p>
+                   <img src={disney} alt='Disney Kids API Project'/>
+                </div>
+                
+                <div className='wrapper'>
+                    <p>hello</p>
+                   <img src={animals} alt='Save The Animals Project'/>
+                </div>
+            </div>
         </section>
     )
 }
