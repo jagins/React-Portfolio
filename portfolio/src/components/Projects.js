ce36d60a996cd588bc9d85b3988b689cebfb2c86
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-07-31 15:25:37 -0500

    projects styled might switch out the button for icons to site and github

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -10,19 +10,26 @@ function Projects()
             <h3>CHECK OUT SOME OF MY WORK</h3>
             <div className='project-container'>
                 <div className='wrapper'>
-                    <p>hello</p>
-                   <img src={earthmed} alt='Earthmed Project'/>
+                    <p>Earthmed Concept Website</p>
+                    <p className='project-desc'>Full Stack eCommerce website for legalized recreational marijuana dispensary sales. </p>
+                    <img src={earthmed} alt='Earthmed Concept Website'/>
+                    <button>Go to deployed site</button>
+                </div>
+
+                <div className='wrapper'>
+                    <p>Disney Kids API</p>
+                    <p className='project-desc'>Disney Kids is a platform for parents to book baby sitting services from other parents or by approved child care providers at any of the Disney parks. </p>
+                    <img src={disney} alt='Disney Kids API'/>
+                    <button>Go to Github</button>
+                </div>
+
+                <div className='wrapper'>
+                    <p>Save The Animals</p>
+                    <p className='project-desc'>Save the Animals is a platform for animal rights organizations to help  raise money for the organization's causes.  </p>
+                    <img src={animals} alt='Disney Kids API'/>
+                    <button>Go to deployed site</button>
                 </div>
                 
-                <div className='wrapper'>
-                    <p>hello</p>
-                   <img src={disney} alt='Disney Kids API Project'/>
-                </div>
-                
-                <div className='wrapper'>
-                    <p>hello</p>
-                   <img src={animals} alt='Save The Animals Project'/>
-                </div>
             </div>
         </section>
     )
