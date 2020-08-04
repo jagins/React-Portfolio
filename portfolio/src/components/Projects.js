9368f3c22ebb6e3c768162ed3b4df596dd13d5f6
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-08-04 12:14:12 -0500

    removed ugly projects button for now

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import earthmed from '../images/portfolio/earthmed.JPG';
 import disney from '../images/portfolio/disney.png';
 import animals from '../images/portfolio/animals.JPG';
+import {FaLink, FaGithub} from 'react-icons/fa'
 
 function Projects()
 {
@@ -13,21 +14,18 @@ function Projects()
                     <p>Earthmed Concept Website</p>
                     <p className='project-desc'>Full Stack eCommerce website for legalized recreational marijuana dispensary sales. </p>
                     <img src={earthmed} alt='Earthmed Concept Website'/>
-                    <button>Go to deployed site</button>
                 </div>
 
                 <div className='wrapper'>
                     <p>Disney Kids API</p>
                     <p className='project-desc'>Disney Kids is a platform for parents to book baby sitting services from other parents or by approved child care providers at any of the Disney parks. </p>
                     <img src={disney} alt='Disney Kids API'/>
-                    <button>Go to Github</button>
                 </div>
 
                 <div className='wrapper'>
                     <p>Save The Animals</p>
                     <p className='project-desc'>Save the Animals is a platform for animal rights organizations to help  raise money for the organization's causes.  </p>
                     <img src={animals} alt='Disney Kids API'/>
-                    <button>Go to deployed site</button>
                 </div>
                 
             </div>
